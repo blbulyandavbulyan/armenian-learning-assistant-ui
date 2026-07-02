91b32d0be6a9ae1e6c74c27444ef48a76c750740
David Blbulyan <blbulyandavbulyan@gmail.com>
2026-07-02 15:09:13 +0400

    deleted unused classes

--- shared/src/commonMain/kotlin/com/blbulyandavbulyan/larm/kmp/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.blbulyandavbulyan.larm.kmp
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return sayHello(platform.name)
-    }
-}
--- shared/src/commonMain/kotlin/com/blbulyandavbulyan/larm/kmp/GreetingUtil.kt
+++
@@ -1,4 +0,0 @@
-package com.blbulyandavbulyan.larm.kmp
-
-fun sayHello(to: String): String =
-    "Hello, $to!"
