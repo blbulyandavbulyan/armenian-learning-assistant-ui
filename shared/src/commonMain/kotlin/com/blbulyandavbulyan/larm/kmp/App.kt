4877d1a59af065aa186ac22348d699c7f316f34c
David Blbulyan <blbulyandavbulyan@gmail.com>
2026-07-03 12:27:05 +0400

    refactor: extract HTTP client and dependencies to AppModule

--- shared/src/commonMain/kotlin/com/blbulyandavbulyan/larm/kmp/App.kt
+++ shared/src/commonMain/kotlin/com/blbulyandavbulyan/larm/kmp/App.kt
@@ -1,45 +1,20 @@
 package com.blbulyandavbulyan.larm.kmp
 
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
-import com.blbulyandavbulyan.larm.kmp.network.ApiClient
-import com.blbulyandavbulyan.larm.kmp.network.NetworkDialogueRepository
+import com.blbulyandavbulyan.larm.kmp.di.AppModule
 import com.blbulyandavbulyan.larm.kmp.ui.DialogueGeneratorScreen
 import com.blbulyandavbulyan.larm.kmp.presentation.DialogueViewModel
-
 import com.blbulyandavbulyan.larm.kmp.ui.theme.ArmenianLearningTheme
-
-import io.ktor.client.*
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.client.plugins.defaultRequest
-import io.ktor.serialization.kotlinx.json.*
-import kotlinx.serialization.json.Json
 
 @Composable
 @Preview
 fun App() {
     ArmenianLearningTheme {
-        val httpClient = remember {
-            HttpClient {
-                install(ContentNegotiation) {
-                    json(Json {
-                        ignoreUnknownKeys = true
-                        prettyPrint = true
-                    })
-                }
-                defaultRequest {
-                    val baseUrl = BuildKonfig.BASE_URL
-                    if (baseUrl.isNotBlank()) {
-                        url(baseUrl)
-                    }
-                }
-            }
-        }
-        val apiClient = remember { ApiClient(httpClient) }
-        val repository = remember { NetworkDialogueRepository(apiClient) }
-        val viewModel = remember { DialogueViewModel(repository) }
+        // ViewModel should ideally be injected or created by a factory
+        // Using remember here is acceptable for ViewModel instance caching until a formal DI library is used
+        val viewModel = remember { DialogueViewModel(AppModule.dialogueRepository) }
         
         DialogueGeneratorScreen(viewModel)
     }
