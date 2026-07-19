c8e11181ad2ea78f206a2f40f8c75e3dfeab119d
david <blbulyandavbulyan@gmail.com>
2026-07-19 20:31:42 +0400

    Add test case for AssetRepository 500 errors

--- shared/src/commonTest/kotlin/com/blbulyandavbulyan/larm/kmp/network/AssetRepositoryTest.kt
+++ shared/src/commonTest/kotlin/com/blbulyandavbulyan/larm/kmp/network/AssetRepositoryTest.kt
@@ -1,5 +1,6 @@
 package com.blbulyandavbulyan.larm.kmp.network
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.shouldBe
 import io.ktor.client.*
 import io.ktor.client.engine.mock.*
@@ -11,7 +12,7 @@ import kotlin.test.Test
 class AssetRepositoryTest {
     @Test
     fun testGetAudioBytes() = runTest {
-        val mockEngine = MockEngine { request ->
+        val mockEngine = MockEngine { _ ->
             respond(
                 content = byteArrayOf(1, 2, 3),
                 status = HttpStatusCode.OK,
@@ -20,6 +21,7 @@ class AssetRepositoryTest {
         }
         val mockClient = HttpClient(mockEngine) {
             install(HttpCache)
+            expectSuccess = true
         }
         val apiClient = ApiClient(client = mockClient)
         val repository = NetworkAssetRepository(apiClient)
@@ -33,5 +35,25 @@ class AssetRepositoryTest {
         result2[0] shouldBe 1.toByte()
 
         mockEngine.requestHistory.size shouldBe 1
+    }
+
+    @Test
+    fun testGetAudioBytesThrowsAudioFetchExceptionOn500() = runTest {
+        val mockEngine = MockEngine { _ ->
+            respond(
+                content = "Internal Server Error",
+                status = HttpStatusCode.InternalServerError
+            )
+        }
+        val mockClient = HttpClient(mockEngine) {
+            install(HttpCache)
+            expectSuccess = true
+        }
+        val apiClient = ApiClient(client = mockClient)
+        val repository = NetworkAssetRepository(apiClient)
+
+        shouldThrow<AudioFetchException> {
+            repository.getAssetBytes("http://example.com/audio.mp3")
+        }
     }
 }
