c33907108470f0408c9e5af4602a2b5b38ebf6a5
david <blbulyandavbulyan@gmail.com>
2026-07-26 00:10:28 +0400

    ChatDomainModels.kt reformatted code

--- shared/src/commonMain/kotlin/com/blbulyandavbulyan/larm/kmp/domain/model/dialogue/chat/ChatDomainModels.kt
+++ shared/src/commonMain/kotlin/com/blbulyandavbulyan/larm/kmp/domain/model/dialogue/chat/ChatDomainModels.kt
@@ -3,8 +3,31 @@ package com.blbulyandavbulyan.larm.kmp.domain.model.dialogue.chat
 import kotlinx.collections.immutable.ImmutableList
 
 data class ChatTranslation(val translationText: String, val isoLanguageCode: String)
-data class DialogueTitle(val text: String, val transcription: String, val translations: ImmutableList<ChatTranslation>)
-data class DraftSpeaker(val id: String, val name: String, val transcription: String, val translations: ImmutableList<ChatTranslation>)
-data class DraftPhrase(val text: String, val isoLanguageCode: String, val transcription: String, val translations: ImmutableList<ChatTranslation>)
+
+data class DialogueTitle(
+    val text: String,
+    val transcription: String,
+    val translations: ImmutableList<ChatTranslation>
+)
+data class DraftSpeaker(
+    val id: String,
+    val name: String,
+    val transcription: String,
+    val translations: ImmutableList<ChatTranslation>
+)
+
+data class DraftPhrase(
+    val text: String,
+    val isoLanguageCode: String,
+    val transcription: String,
+    val translations: ImmutableList<ChatTranslation>
+)
+
 data class DraftDialoguePhrase(val speakerId: String, val phrase: DraftPhrase)
-data class GeneratedDialogue(val message: String, val info: DialogueTitle, val speakers: ImmutableList<DraftSpeaker>, val phrases: ImmutableList<DraftDialoguePhrase>)
+
+data class GeneratedDialogue(
+    val message: String,
+    val info: DialogueTitle,
+    val speakers: ImmutableList<DraftSpeaker>,
+    val phrases: ImmutableList<DraftDialoguePhrase>
+)
