2b94bed521f7652be26bd8fc947c4219875e7cfd
david <blbulyandavbulyan@gmail.com>
2026-08-08 23:29:53 +0400

    added AudioTest

---
+++ shared/src/commonTest/kotlin/com/blbulyandavbulyan/larm/kmp/infrastructure/audio/AudioTest.kt
@@ -0,0 +1,48 @@
+package com.blbulyandavbulyan.larm.kmp.infrastructure.audio
+
+import io.kotest.matchers.shouldBe
+import kotlin.test.Test
+
+class AudioTest {
+
+    @Test
+    fun equals_sameInstance_returnsTrue() {
+        val audio = Audio(byteArrayOf(1, 2, 3), "audio/mp3")
+        (audio == audio) shouldBe true
+    }
+
+    @Test
+    fun equals_sameContentAndMimeType_returnsTrue() {
+        val audio1 = Audio(byteArrayOf(1, 2, 3), "audio/mp3")
+        val audio2 = Audio(byteArrayOf(1, 2, 3), "audio/mp3")
+        (audio1 == audio2) shouldBe true
+    }
+
+    @Test
+    fun equals_nullOrDifferentClass_returnsFalse() {
+        val audio = Audio(byteArrayOf(1, 2, 3), "audio/mp3")
+        audio.equals(null) shouldBe false
+        audio.equals("not an audio") shouldBe false
+    }
+
+    @Test
+    fun equals_differentData_returnsFalse() {
+        val audio1 = Audio(byteArrayOf(1, 2, 3), "audio/mp3")
+        val audio2 = Audio(byteArrayOf(4, 5, 6), "audio/mp3")
+        (audio1 == audio2) shouldBe false
+    }
+
+    @Test
+    fun equals_differentMimeType_returnsFalse() {
+        val audio1 = Audio(byteArrayOf(1, 2, 3), "audio/mp3")
+        val audio2 = Audio(byteArrayOf(1, 2, 3), "audio/wav")
+        (audio1 == audio2) shouldBe false
+    }
+
+    @Test
+    fun hashCode_equalObjects_returnsSameHashCode() {
+        val audio1 = Audio(byteArrayOf(1, 2, 3), "audio/mp3")
+        val audio2 = Audio(byteArrayOf(1, 2, 3), "audio/mp3")
+        audio1.hashCode() shouldBe audio2.hashCode()
+    }
+}
